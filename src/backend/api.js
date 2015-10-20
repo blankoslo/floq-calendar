b7bd51efa555fbdb2760b74dd06175c565829d5b
Martin Gammelsæter <martin@mg.am>
2015-10-20 13:22:20 +0200

    Move range into /absence_days endpoint.
    Also start work on PUT for same endpoint.

--- src/backend/api.js
+++ src/backend/api.js
@@ -24,6 +24,43 @@ api.get('/absence_types', function(req, res) {
 // None of the absence_days endpoints use the typical /absence_days/:id-pattern,
 // since we support doing operations on multiple days at once.
 
+api.post('/absence_days', function(req, res) {
+    var success = function(qRes) {
+        res.json({success: true, data: qRes.rows[0]});
+    }
+
+    var failure = function(err) {
+        console.log('Unable to query absence days:', err);
+        res.status(500).json({success: false, data: err});
+    }
+
+    var data = req.body;
+
+    // Dealing with a single date.
+    if (req.body.date) {
+        db.singleQuery('INSERT INTO absence_days(employee, type, date)'
+                // TODO: Perhaps we should return the full row.
+                + ' VALUES ($1, $2, $3) RETURNING id',
+                [data.employee, data.absence_type, data.date])
+                    .then(success, failure);
+        return;
+    // Dealing with a date range.
+    } else if (req.body.fromDate && req.body.toDate) {
+        db.singleQuery('INSERT INTO absence_days(employee, type, date)'
+                + ' SELECT $1, $2, d::date'
+                + ' FROM generate_series($3::date, $4, \'1 day\') AS d'
+                + ' RETURNING id',
+                [data.employee, data.absence_type, data.fromDate, data.toDate])
+                    .then(success, failure);
+        return;
+    }
+
+    // TODO: Do a more complete check for well-formed requests.
+    res.sendStatus(400);
+    console.log('Received bad request.')
+});
+
+
 api.get('/absence_days', function(req, res) {
     // TODO: `success` and `failure` are equal in all calls.
     // Pull out as "middleware"?
@@ -41,7 +78,7 @@ api.get('/absence_days', function(req, res) {
     // TODO: Support ranges.
     // TODO: Should always constrain to range anyways, so...
     var query = 'SELECT * FROM absence_days';
-    var paramList = []
+    var paramList = [];
     if (data.employee) {
         query += ' WHERE employee = $1';
         paramList.push(data.employee);
@@ -53,16 +90,10 @@ api.get('/absence_days', function(req, res) {
     db.singleQuery(query, paramList).then(success, failure);
 });
 
-api.post('/absence_days', function(req, res) {
-    // TODO: Do a more complete check for well-formed requests.
-    if (!req.body.date) {
-        res.sendStatus(400);
-        console.log('Received bad request.')
-        return;
-    }
-
+/*
+api.put('/absence_days', function(req, res) {
     var success = function(qRes) {
-        res.json({success: true, data: qRes.rows[0]});
+        res.json({success: true, data: qRes.rows});
     }
 
     var failure = function(err) {
@@ -70,13 +101,9 @@ api.post('/absence_days', function(req, res) {
         res.status(500).json({success: false, data: err});
     }
 
-    var data = req.body;
-    db.singleQuery('INSERT INTO absence_days(employee, type, date)'
-            // TODO: Perhaps we should return the full row.
-            + ' VALUES ($1, $2, $3) RETURNING id',
-            [data.employee, data.absence_type, data.date])
-                .then(success, failure);
+    var dat
 });
+*/
 
 api.delete('/absence_days', function(req, res) {
     // TODO: Do a more complete check for well-formed requests.
@@ -103,33 +130,6 @@ api.delete('/absence_days', function(req, res) {
     db.singleQuery(
             'DELETE FROM absence_days WHERE id = ANY($1::int[]) RETURNING id',
             [data.id])
-                .then(success, failure);
-});
-
-// TODO: Move into absence_days endpoint instead?
-api.post('/absence_days/range', function(req, res) {
-    // TODO: Do a more complete check for well-formed requests.
-    if (!req.body.fromDate || !req.body.toDate) {
-        res.sendStatus(400);
-        console.log('Received bad request.')
-        return;
-    }
-
-    var success = function(qRes) {
-        res.json({success: true, data: qRes.rows});
-    }
-
-    var failure = function(err) {
-        console.log('Unable to query absence days:', err);
-        res.status(500).json({success: false, data: err});
-    }
-
-    var data = req.body;
-    db.singleQuery('INSERT INTO absence_days(employee, type, date)'
-            + ' SELECT $1, $2, d::date'
-            + ' FROM generate_series($3::date, $4, \'1 day\') AS d'
-            + ' RETURNING id',
-            [data.employee, data.absence_type, data.fromDate, data.toDate])
                 .then(success, failure);
 });
 
