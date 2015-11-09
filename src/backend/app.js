b2862b02414abaab26a6ebf8aa7a217edf4a4063
Magne Davidsen <magne.davidsen@gmail.com>
2015-11-09 14:58:58 +0100

    picking up port from env

--- src/backend/app.js
+++ src/backend/app.js
@@ -10,7 +10,9 @@ app.get('/', function (req, res) {
 app.use('/api', require('./api'));
 app.use('/static', express.static('../frontend/dist/'));
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
     var host = server.address().address;
     var port = server.address().port;
 
