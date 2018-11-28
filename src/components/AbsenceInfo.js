ba33c7095b51433bb3799dc13bc52e65492ea393
Kristiane Westgård <kristiane.westgard@blank.no>
2018-11-28 14:47:15 +0100

    made info container more responsive

--- src/components/AbsenceInfo.js
+++ src/components/AbsenceInfo.js
@@ -34,28 +34,26 @@ class AbsenceInfo extends React.PureComponent {
   render() {
     return (
       <div className='info'>
+        <div className='year-selector'>
+          <IconButton
+            iconClassName='material-icons'
+            onClick={this.props.prevYear}
+            iconStyle={{ fontSize: 12, color: '#3c1345' }}
+          >
+            arrow_back
+              </IconButton>
+          <h1 className={'year-selector-text'}>
+            {this.props.year.toString()}
+          </h1>
+          <IconButton
+            iconClassName='material-icons'
+            onClick={this.props.nextYear}
+            iconStyle={{ fontSize: 12, color: '#3c1345' }}
+          >
+            arrow_forward
+              </IconButton>
+        </div>
         <div className='info-box-container'>
-          <div className='info-box'>
-            <div className='year-selector'>
-              <IconButton
-                iconClassName='material-icons'
-                onClick={this.props.prevYear}
-                iconStyle={{ fontSize: 12, color: '#3c1345' }}
-              >
-                arrow_back
-              </IconButton>
-              <h1 className={'year-selector-text'}>
-                {this.props.year.toString()}
-              </h1>
-              <IconButton
-                iconClassName='material-icons'
-                onClick={this.props.nextYear}
-                iconStyle={{ fontSize: 12, color: '#3c1345' }}
-              >
-                arrow_forward
-              </IconButton>
-            </div>
-          </div>
           <div className='info-box'>
             <h6 className='info-header'> Tidligere Fravær </h6>
             <ul className='info-list'>
